8d9eb0ee9da0ea34027efcf24b8b84858c9568b0
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2022-04-23 17:53:35 +0530

    pattern questions

--- pattern.java
+++ pattern.java
@@ -2,9 +2,41 @@ package com.company;
 
 public class pattern {
     public static void main(String[] args) {
-
+        pattern3(7);
     }
-    static  void pattern(int n){
-
+    static  void pattern1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n - i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
-}
+    static  void pattern2(int n){
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <=i ; j++) {
+                    System.out.print(j + " ");
+                }
+                System.out.println("");
+            }
+        }
+    static  void pattern3(int n){
+        int mid = (n/2)+1;
+        for (int i = 1; i <=mid ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        for (int i = 1; i <=mid-1 ; i++) {
+            for (int j = mid-i ; j >0 ; j--) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+    }
+

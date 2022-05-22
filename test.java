3b2182a5553481340a952461c39864c5f1174d73
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2022-05-22 16:11:55 +0530

    bitwise

--- test.java
+++ test.java
@@ -2,9 +2,9 @@ package com.company;
 
 class test{
     public static void main(String[] args) {
-        int n = 13;
-        int i=3;
-        System.out.println((n&(1<<i-1))>>i-1);
+        int n = 31;
+        int ans = n & (n);
+        System.out.println(ans);
 
     }
 }

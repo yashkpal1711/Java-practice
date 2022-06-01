a0dac6f97df11c6027ce91a299e9243070fa7eea
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2022-06-01 17:18:20 +0530

    maths for DSA

---
+++ FactorsOfNo.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class FactorsOfNo {
+    public static void main(String[] args) {
+        int n = 36;
+        factors(n);
+    }
+
+    static void factors(int n){
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= Math.sqrt(n) ; i++) {
+                if(n%i==0){
+                    if(n/i==i){
+                        System.out.print(i+",");
+                    }else {
+                        System.out.print(i+",");
+                        list.add(n/i);
+                    }
+                }
+        }
+        for (int i = list.size()-1; i >=0 ; i--) {
+            System.out.print(list.get(i)+",");
+        }
+    }
+}

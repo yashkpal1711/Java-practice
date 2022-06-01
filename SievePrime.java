a1b7f8e31b581f19ddcfa2fb0b1a24d3ce428710
yashkpal1711 <69109847+yashkpal1711@users.noreply.github.com>
2022-06-01 12:11:58 +0530

    maths for DSA

--- SievePrime.java
+++ SievePrime.java
@@ -2,6 +2,23 @@ package com.company;
 
 public class SievePrime {
     public static void main(String[] args) {
-        
+        int n = 41 ;
+
+        boolean[] Arr = new boolean[n+1];
+
+        for (int i = 2; i*i <n ; i++) {
+            if(!Arr[i]){
+                for (int j = i*2; j < n; j+=i) {
+                    Arr[j]= true;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if(!Arr[i]){
+                System.out.println(i +" "+"prime");
+            }
+        }
+
     }
 }
